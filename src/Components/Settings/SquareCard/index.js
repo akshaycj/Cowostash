0599311d80498de1eaee6731a3bea7cfed04c337
Akshay Cj <akshaycj999@gmail.com>
2018-08-28 19:01:43 +0530

    Purpose tab items

--- src/Components/Settings/SquareCard/index.js
+++ src/Components/Settings/SquareCard/index.js
@@ -1,17 +1,40 @@
 import React, { Component } from "react";
 import "./index.css";
+import { Icon } from "antd";
 
 export default class extends Component {
   render() {
     return (
-      <div className="square-card">
-        <img
-          src={this.props.src}
-          style={{ alignSelf: "center" }}
-          width={50}
-          height={50}
-        />
-        <div style={{ marginTop: 5 }}>{this.props.title}</div>
+      <div className="square-card-container">
+        <div className="square-card">
+          <img
+            src={this.props.src}
+            style={{ alignSelf: "center" }}
+            width={50}
+            height={50}
+          />
+          <div style={{ marginTop: 5 }}>{this.props.title}</div>
+          <Icon
+            type="check-circle"
+            style={{ alignSelf: "flex-end", marginRight: "auto" }}
+          />
+        </div>
+        <div className="square-card-menu">
+          <div className="square-card-menu-inner">
+            <div className="square-card-menu-inner-circle">
+              <Icon
+                type="check-circle-o"
+                style={{ alignSelf: "center", margin: "auto" }}
+              />
+            </div>
+            <div className="square-card-menu-inner-circle">
+              <Icon
+                type="form"
+                style={{ alignSelf: "center", margin: "auto" }}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
