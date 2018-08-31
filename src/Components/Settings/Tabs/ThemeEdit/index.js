151ef4bf6a7bae77d81e2b476b4b60e7c8552822
notacoolcoder <notacoolcoder@gmail.com>
2018-08-31 19:21:25 +0530

    Tablet theme ui fix 2

--- src/Components/Settings/Tabs/ThemeEdit/index.js
+++ src/Components/Settings/Tabs/ThemeEdit/index.js
@@ -1,70 +1,102 @@
-import React, { Component,Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import './index.css'
 import { Input, Icon } from "antd"
 import SquareCard from "../../SquareCard"
+import theme1 from "../../../../Res/theme1.jpg"
+import theme2 from "../../../../Res/theme2.jpeg"
+import theme3 from "../../../../Res/theme3.jpeg"
+import logo from "../../../../Res/logo.svg"
 
 const { TextArea } = Input;
 export default class extends Component {
-
+  constructor(props) {
+    super(props)
+    this.state = {
+      ipadbackground: theme1,
+      MainText: "Hello",
+      SubText: "Select Purpose of your visit",
+      TextCol: "#000",
+      BackgroundCol: "#A9A9A9"
+    }
+  }
+  onMainText = (e) => {
+    this.setState({ MainText: e.target.value })
+  }
+  onSubText = (e) => {
+    this.setState({ SubText: e.target.value })
+  }
+  onBackgroundColor = (e) => {
+    this.setState({ BackgroundCol: e.target.value })
+  }
+  onTextColor = (e) => {
+    this.setState({ TextCol: e.target.value })
+  }
+  onLogo1 = () => {
+    this.setState({ipadbackground:theme1})
+  }
+  onLogo2 = () => {
+    this.setState({ipadbackground:theme2})
+  }
+  onLogo3 = () => {
+    this.setState({ipadbackground:theme3})
+  }
   render() {
 
     return (
       <div className="Theme-Container" >
 
-        <div>
+        <div style={{ overlayY: "scroll" }}>
           <div className="Theme-padding"><div>Upload Logo</div> <Icon style={{ fontSize: "20px" }} type="cloud-upload" /></div>
-          <div className="Theme-padding"><div>Logo Background Color</div><Input style={{height:"20px",width:"80px"}} placeholder="#000"/></div>
+          <div className="Theme-padding"><div>Logo Background Color</div><Input style={{ height: "20px", width: "80px" }} placeholder="#000" onChange={this.onBackgroundColor} /></div>
           <div className="Theme-padding">Upload Background/Select from Templates</div>
           <div style={{ display: "flex" }}>
-            <div class="pa4">
+            <div class="pa4" onClick={this.onLogo1}>
               <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6yveMXr63G_6KZVKfusBOmFDmY-qZNjWcBU07WKn___3Oy6Ch"
+                src={theme1}
                 class="br-100" alt="avatar" />
-            </div><div class="pa4">
+            </div><div class="pa4" onClick={this.onLogo2}>
               <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSCZOBZzIynldLrIAp2uGAKRy04WmXlkN6eh_iIL5JQcIERugkqQ"
-                class="br-100" alt="avatar"  />
-            </div><div class="pa4">
+                src={theme2}
+                class="br-100" alt="avatar" />
+            </div><div class="pa4" onClick={this.onLogo3}>
               <img
-                src="http://bldgwlf.com/wp-content/uploads/2012/01/danm4.jpg"
+                src={theme3}
                 class="br-100" alt="avatar" />
             </div>
             <Icon style={{ fontSize: "30px", marginTop: "15px" }} type="cloud-upload" />
           </div>
-          <Input className="Theme-padding" placeholder="Main Text" />
-          <TextArea className="Theme-padding" placeholder="Sub Text" autosize={{ minRows: 2, maxRows: 6 }} />
-          <div className="Theme-padding"><div>Text Color</div> <div><Input style={{height:"20px",width:"80px"}}  placeholder="#000"/></div></div>
+          <Input className="Theme-padding" placeholder="Main Text" onChange={this.onMainText} />
+          <TextArea className="Theme-padding" placeholder="Sub Text" onChange={this.onSubText} autosize={{ minRows: 2, maxRows: 6 }} />
+          <div className="Theme-padding"><div>Text Color</div> <div><Input style={{ height: "20px", width: "80px" }} onChange={this.onTextColor} placeholder="#000" /></div></div>
           <div className="Theme-padding "><div>Quick Links</div> <div className="Theme-button-quick">Quick Link +</div></div>
           <hr style={{ width: "320px" }} />
           <div className="Theme-padding" >
             <SquareCard title="Visitor" />
-            <SquareCard style={{ alignItems: "center", justifyContent: "center" }} title="Event" />
+            <SquareCard title="Event" />
+
           </div>
+
         </div>
 
-        
-        
-        <div className="Ipad-Container" ><div className="Ipad-Display"></div><div className="Ipad-Display-Items"><div class="pa4">
-              <img
-                src="http://bldgwlf.com/wp-content/uploads/2012/01/danm4.jpg"
-                class="Ipad-Logo  " alt="avatar" />
-            </div>
-            <h2>Hello</h2>
-            <h5>Select Purpose of your visit</h5>
-            <div style={{display:"flex",marginTop:"-10px"}}><div class="pa4">
-              <img
-                src="http://bldgwlf.com/wp-content/uploads/2012/01/danm4.jpg"
-                class="Ipad-Logo  " alt="avatar" />
-            </div><div class="pa4">
-              <img
-                src="http://bldgwlf.com/wp-content/uploads/2012/01/danm4.jpg"
-                class="Ipad-Logo  " alt="avatar" />
-            </div></div>
-            </div></div>
-        
-      
 
 
+        <div className="Ipad-Container"  ><div className="Ipad-Display" style={{ backgroundImage:"url(" + this.state.ipadbackground + ")" }}></div><div className="Ipad-Display-Items"><div class="pa4" >
+          <img style={{ backgroundColor: this.state.BackgroundCol }}
+            src={logo}
+            class="Ipad-Logo" alt="logo" />
+        </div>
+          <h2 style={{ color: this.state.TextCol }}>{this.state.MainText}</h2>
+          <h5 style={{ color: this.state.TextCol }}>{this.state.SubText}</h5>
+          <div style={{ display: "flex", marginTop: "-10px" }}><div class="pa4">
+            <img
+              src={theme1}
+              class="Ipad-Logo  " alt="avatar" />
+          </div><div class="pa4">
+              <img
+                src={theme1}
+                class="Ipad-Logo  " alt="avatar" />
+            </div></div>
+        </div></div>
       </div>
     );
   }
